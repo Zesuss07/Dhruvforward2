0a16c54ea436e7da5edd6901b87c0bd48580aaa5
Dhruv3838 <145219319+Dhruv3838@users.noreply.github.com>
2025-07-29 08:10:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", "28473509"))
-    API_HASH = environ.get("API_HASH", "f56218a21931d5f4ddcf0f0354256816")
+    API_ID = int(environ.get("API_ID", "24689889"))
+    API_HASH = environ.get("API_HASH", "555ff52a3a0b0ef7ad253a6a82ff5c54")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Anushka") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://jackey172363:KeAhgj2viyWoHrxn@cluster0.s7ydqcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vinod7u")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "8084341411"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "1841380052"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
