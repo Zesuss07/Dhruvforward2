88fab8967d1b4189d8e46db1baa78ec328418797
Dhruv3838 <145219319+Dhruv3838@users.noreply.github.com>
2025-07-29 07:14:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from os import environ 
+
+class Config:
+    API_ID = int(environ.get("API_ID", "28473509"))
+    API_HASH = environ.get("API_HASH", "f56218a21931d5f4ddcf0f0354256816")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Anushka") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://jackey172363:KeAhgj2viyWoHrxn@cluster0.s7ydqcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "vinod7u")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "8084341411"))
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
